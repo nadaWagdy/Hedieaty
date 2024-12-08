322e99b2fca88e655ab918272c4d2c9e1ce1c6c6
nadaWagdy <nawagdy332@gmail.com>
2024-12-08 03:58:27 +0200

    Added enums for models

---
+++ lib/models/enums.dart
@@ -0,0 +1,4 @@
+enum EventStatus { upcoming, current, past }
+enum EventCategory { personal, birthday, wedding, graduation, other }
+enum GiftStatus { available, pledged, purchased }
+enum GiftCategory { electronics, books, clothing, other }
