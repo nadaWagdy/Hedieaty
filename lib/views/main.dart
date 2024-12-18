ba16221d0636580f1e361a78146e8a1748aa5beb
nadaWagdy <nawagdy332@gmail.com>
2024-12-18 06:32:37 +0200

    Added notifications initialization in main

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hedieaty/services/notification_service.dart';
 import 'home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await NotificationService().initNotifications();
   runApp(const HedieatyApp());
 }
