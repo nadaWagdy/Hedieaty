920ad6e4734640395e10bc3aea3955e3450a4855
nadaWagdy <nawagdy332@gmail.com>
2024-12-20 08:12:12 +0200

    Updated models

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,6 +1,3 @@
-import 'package:firebase_database/firebase_database.dart';
-import 'package:sqflite/sqflite.dart';
-import '../services/db_service.dart';
 import 'gift.dart';
 import 'enums.dart';
 
@@ -70,66 +67,5 @@ class Event {
       status: EventStatus.values.firstWhere((e) => e.name == map['status']),
       category: EventCategory.values.firstWhere((e) => e.name == map['category']),
     );
-  }
-
-  //SQLite
-  static Future<void> saveDraft(Event event) async {
-    final db = await DatabaseService().database;
-    await db.insert('DraftEvents', event.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  static Future<List<Event>> getDrafts() async {
-    final db = await DatabaseService().database;
-    final maps = await db.query('DraftEvents');
-    return maps.map((map) => Event.fromMap(map)).toList();
-  }
-
-  //Firebase
-  static Future<void> publishToFirebase(Event event, String userId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events').push();
-    event.id = ref.key ?? '';
-    await ref.set(event.toFirebaseMap());
-  }
-
-  static Future<List<Event>> fetchFromFirebase(String userId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events');
-    final snapshot = await ref.get();
-
-    if (snapshot.exists && snapshot.value is Map) {
-      final eventsMap = snapshot.value as Map<dynamic, dynamic>;
-      return eventsMap.values
-          .map((e) => Event.fromFirebaseMap(Map<String, dynamic>.from(e)))
-          .toList();
-    }
-    return [];
-  }
-
-  static Future<void> deleteFromFirebase(String userId, String eventId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventId');
-    await ref.remove();
-  }
-
-  Future<void> updateInFirebase(String userId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$id');
-    await ref.update(toFirebaseMap());
-  }
-
-  static Future<DateTime?> getEventDateById(String userId, String eventId) async {
-    try {
-      final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventId');
-      final snapshot = await ref.get();
-
-      if (snapshot.exists && snapshot.value is Map) {
-        final data = Map<String, dynamic>.from(snapshot.value as Map);
-        if (data['date'] != null) {
-          return DateTime.parse(data['date']);
-        }
-      }
-      return null;
-    } catch (e) {
-      print("Error fetching event date: $e");
-      return null;
-    }
   }
 }
--- lib/models/gift.dart
+++ lib/models/gift.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:hedieaty/models/enums.dart';
-import 'package:sqflite/sqflite.dart';
-import '../services/db_service.dart';
+
 
 class Gift {
   String id;
@@ -92,68 +90,6 @@ class Gift {
       }).toList();
     }
     return [];
-  }
-
-
-  //SQLite
-  static Future<void> saveDraft(Gift gift) async {
-    final db = await DatabaseService().database;
-    await db.insert('DraftGifts', gift.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  static Future<List<Gift>> getDrafts() async {
-    final db = await DatabaseService().database;
-    final maps = await db.query('DraftGifts');
-    return maps.map((map) => Gift.fromMap(map)).toList();
-  }
-
-  //Firebase
-  static Future<void> publishToFirebase(Gift gift, String eventID, String userId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventID/gifts').push();
-    gift.id = ref.key ?? '';
-    await ref.set(gift.toFirebaseMap());
-  }
-
-  static Future<List<Gift>> fetchFromFirebase(String eventID, String userId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventID/gifts');
-    final snapshot = await ref.get();
-
-    if (snapshot.exists) {
-      final Map<String, dynamic> giftsMap = Map<String, dynamic>.from(snapshot.value as Map<Object?, Object?>);
-      return parseGifts(giftsMap);
-    }
-    return [];
-  }
-
-  static Future<Gift?> getGiftById(String userId, String eventId, String giftId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventId/gifts/$giftId');
-    final snapshot = await ref.get();
-
-    if (snapshot.exists) {
-      final Map<String, dynamic> giftMap = Map<String, dynamic>.from(snapshot.value as Map<Object?, Object?>);
-      return Gift.fromFirebaseMap(giftMap);
-    }
-    return null;
-  }
-
-
-  static Future<void> updateStatus(String userId, String eventId, String giftId, GiftStatus newStatus, String pledgedBy) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventId/gifts/$giftId');
-    await ref.update({
-      'status': newStatus.name,
-      'pledged_by' : pledgedBy
-    });
-  }
-
-  Future<void> updateInFirebase(String userId, String eventId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventId/gifts/$id');
-    await ref.update(toFirebaseMap());
-  }
-
-  static Future<void> deleteFromFirebase(String userId, String eventId, String giftId) async {
-    final ref = FirebaseDatabase.instance.ref('users/$userId/events/$eventId/gifts/$giftId');
-    await ref.remove();
   }
 
 }
