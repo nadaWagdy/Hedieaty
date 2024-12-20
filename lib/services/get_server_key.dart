35a3a9a45d7c63819fcd06be124c14a85b0c1c1d
nadaWagdy <nawagdy332@gmail.com>
2024-12-20 22:50:14 +0200

    Get Server Key Service

---
+++ lib/services/get_server_key.dart
@@ -0,0 +1,19 @@
+import 'package:googleapis_auth/auth_io.dart';
+import 'credentials.dart';
+
+class GetServerKey {
+  Future<String> getServerKeyToken() async {
+    final scopes = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/firebase.database',
+      'https://www.googleapis.com/auth/firebase.messaging',
+    ];
+
+    final client = await clientViaServiceAccount(
+      ServiceAccountCredentials.fromJson(credentials),
+      scopes,
+    );
+    final accessServerKey = client.credentials.accessToken.data;
+    return accessServerKey;
+  }
+}
